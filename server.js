6d2f3a941867e96f023208cdb9db11f0035d153e
Matthew Lane <themplane@gmail.com>
2020-11-19 14:29:06 -0800

    Create a system of storing userIDs on connect and deletion on disconnent.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const server = require('http').createServer(app);
+let users = [];
 
 const io = require("socket.io")(server, {
   cors: {
@@ -14,15 +15,18 @@ app.use(express.json());
 app.use(cors());
 app.set('port', process.env.PORT || 3001);
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next()
-});
-
-io.on( "connect", ( socket ) => {
+io.on( 'connect', ( socket ) => {
   console.log('A user has connected!');
-})
+  users.push(socket.id)
+  console.log(`Current Users: ${users}`)
+
+  socket.on('disconnect', (socket) => {
+    console.log('A user has disconnected!');
+    let i = users.indexOf(socket.id)
+    users.splice(i, 1);
+    console.log(`Current Users: ${users}`)
+  });
+});
 
 server.listen(app.get('port'), () => {
   console.log(`Listening on port ${app.get('port')}.`);
