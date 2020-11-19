b73afda1cdd9b8cc093b7394309d9693ae68c076
Matthew Lane <themplane@gmail.com>
2020-11-19 14:13:17 -0800

    Implement CORS access.

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const app = express();
-const server = require('http').createServer(app);
-const io = require('socket.io')(server);
 const cors = require('cors');
+const server = require('http').createServer(app);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: true,
+    methods: ["GET", "POST"]
+  }
+});
 
 app.use(express.json());
 app.use(cors());
 app.set('port', process.env.PORT || 3001);
+
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
 
 io.on( "connect", ( socket ) => {
   console.log('A user has connected!');
